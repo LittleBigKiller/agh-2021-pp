1a2cf35afd41de7895e82386791670010bc6229c
LittleBigKiller <35974496+LittleBigKiller@users.noreply.github.com>
2021-04-25 02:46:53 +0200

    Zadania 2021-04-23

---
+++ 2021-04-23/zad2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <malloc.h>
+
+int rom_to_dec(char*, int);
+
+int main(void) {
+    fgets();
+
+    return 0;
+}
+
+int rom_to_dec(char* rom_tab, int size) {
+    int val = 0;
+
+//    for (int i = 0; i < size; i++) {
+//        if rom_tab[i] = "";
+//    }
+
+
+    return val;
+}
+
